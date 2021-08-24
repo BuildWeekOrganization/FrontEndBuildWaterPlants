6ca186244cb3350ad52b23dd3936b8ba09a5b89f
Retrofitt <rafaelmendozajr94@gmail.com>
2021-08-24 10:05:20 -0700

    Functional Form Page w/ validation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { Route, Link, Switch } from 'react-router-dom';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/signup'>Sign up</Link>
+      <Switch>
+        <Route path={'/signup'}>
+          <SignUp />
+        </Route>
+      </Switch>
     </div>
   );
 }

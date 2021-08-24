834af4f73e3958140a441cb9158be4479ad3c52c
billmyers223 <billymyers223@gmail.com>
2021-08-23 20:38:29 -0500

    Finished up basic styling and code can be pulled to create the sign up component

--- my-app/src/components/LoginForm.js
+++ my-app/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-
+import { Link} from 'react-router-dom';
 
 function LoginForm({LoginF, error}) {
     const [details, setDetails] = useState({email:'', password: ''})
@@ -12,8 +12,8 @@ function LoginForm({LoginF, error}) {
     return (
         <form onSubmit = {submitHandler}>
             <div className ='form-card'>
-                <h2>Login</h2>
-                {(error != '') ? (<div className = 'error'>{error}</div>) : ''}
+                <h2 className = 'login'>Login</h2>
+                {(error !== '') ? (<div className = 'error'>{error}</div>) : ''}
                 <div className ='form-group'>
                     <label htmlFor ='email'>E-mail: </label>
                     <input type ='email' name ='email' id = 'email' onChange ={e => setDetails({...details, email: e.target.value})} value ={details.email}/>
@@ -22,7 +22,8 @@ function LoginForm({LoginF, error}) {
                     <label htmlFor ='password'>Password: </label>
                     <input type ='password' name ='password' id = 'password' onChange ={e => setDetails({...details, password: e.target.value})} value ={details.password}/>
                 </div>
-                <input type = 'submit' value ='LOGIN'/>
+                <input type = 'submit' value ='LOGIN' className='submit'/>
+                <Link to ='/'><a>Don't have an account? Sign up!</a></Link>
             </div>
         </form>
     )

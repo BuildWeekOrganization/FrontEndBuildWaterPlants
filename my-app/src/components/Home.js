9879c4678de87e2c2f8602744a0570fde53cc698
Bradford-Meziere <coleokc@gmail.com>
2021-08-27 03:08:35 -0500

    Added dashboard component, PlantCreation so far

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Dashboard from './Dashboard';
+import './styles/login.css';
 
 function Home(){
 return (
 
     <div className="App">
       <div className='container'>
-        
+        <div>
+          <h1>Welcome to Water My Plants!</h1>
+          <h3>Our goal is to create an easy way for our users to keep track of how often to water their plants!</h3>
+        </div>
       </div>
     </div>
 
---
+++ my-app/src/components/PlantPanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PlantPanel(props) {
+
+    return (
+        <div>
+            <h2>{props.plant.nickname}</h2>
+            <img src={props.plant.image} alt={props.plant.nickname} />
+            <p> {props.plant.nickname} is a {props.plant.species}.</p>
+            <p> Make sure {props.plant.nickname} is watered every {props.plant.h2o_frequency} days!</p>
+        </div>
+    )
+}
